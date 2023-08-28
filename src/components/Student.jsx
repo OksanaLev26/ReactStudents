c56a293c6e2633f8aa4637638d357ce8f10921c4
oksananaumova <oksana.naumova@mustpay.tech>
2023-08-28 15:17:37 -0400

    students

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,13 +1,13 @@
 import Score from "./Score";
-import './student.css';
+import "./student.css";
 
-export default function Student({student}) {
+export default function Student({ student }) {
   return (
-    <div className='student'>
-        <h1>{student.name}</h1>
-        <p>{student.bio}</p>
-        <h2>Student Scores</h2>
-            <Score scores={student.scores} />
+    <div className="student">
+      <h1>{student.name}</h1>
+      <p>{student.bio}</p>
+      <h2>Student Scores</h2>
+      <Score scores={student.scores} />
     </div>
-  )
+  );
 }
